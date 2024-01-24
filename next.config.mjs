09b5614a11f48d0f65313690574bc54661bbbe37
imen yousfi <imenyousfi44@gmail.com>
2024-01-24 16:43:09 +0100

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+// next.config.mjs
+export default {
+    // Configurations spécifiques de Next.js vont ici.
+    // Par exemple, la configuration du répertoire de sortie (distDir).
+    distDir: 'out',
+  };
+  
